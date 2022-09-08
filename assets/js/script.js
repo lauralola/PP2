4d5867818e2d88d2d934a8dfcb36d9a1068a9625
lauralola <laura6289@gmail.com>
2022-09-08 21:10:26 +0000

    Added function for endGame if player clicks hold

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,138 +8,62 @@ document.addEventListener("DOMContentLoaded", function(){
 
     for (let button of buttons){
         button.addEventListener('click', function(){
-            if (this.getAttribute('data-type')=== 'hit'){
-                getNumber();
+            if (this.getAttribute('data-type') === 'hit'){
+                gamePlay();
             } else {
                 alert('Hold')
+                endGame();
             }
         })
     }
 
-    getNumber();
+    gamePlay();
 })
-
-let clickstate = 0;
 
 let player1=0;
 
 let player2=0;
 
-function getNumber(){
+function play1(){
 
     let num1 =  Math.floor(Math.random() * 12) +1; 
-    let num2 =  Math.floor(Math.random() * 12) +1; 
-    console.log(`The random number is ${num1} and ${num2}`);
     player1 = player1 + num1;
+    return player1;
+}
+    
+function play2(){
+    let num2 =  Math.floor(Math.random() * 12) +1; 
     player2 = player2 + num2;
-    console.log(`Player 1 score is ${player1}`)
-    console.log(`Player 2 score is ${player2}`)
-    return values;
+    return player2;
 }
 
 function gamePlay(){
-    do {
-    getNumber()
-} while (player1 <= 21)
+    play1();
+    document.getElementsByClassName('player').textContent = player1;
+    if (player2 <=19){
+        play2()} else {
+            alert('Computer Holds!')
+            play1();
+        }
 }
 
-gamePlay();
-
-/**
- * Main game loop after user has played- shuffles 12 numbers and records clicks on hit me button- 
- * For 1st hitme should trigger fxn to deal card 1 and 2 etc
- * For hold should trigger hold fxn
- */
-// function runGame(){
-
-//     let values=['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
-//     function shuffle(values){return values.sort (()=> Math.random() -0.5)};
-
-//     shuffle(values);
-
-//     let clickState = 0;
-
-//     function hitMe(){
-//         let clickState = 0;
-//         clickState++;
-//         if (clickState==1){
-//             function hitMe1()
-//             } else if (clickState ==2){
-//             function hitMe2()
-//             } else if (clickState ==3){
-//             function hitMe3()
-//             } else if (clickState ==4){
-//             function hitMe4()}
-//             else {
-//             alert('Hold');
-//             function hold()
-//             }
-
-//     // // creates 8 random numbers between 1 and 12
-//     // let num1= Math.floor(Math.random()*12)+1;
-//     // let num2= Math.floor(Math.random()*12)+1;
-//     // let num3= Math.floor(Math.random()*12)+1;
-//     // let num4= Math.floor(Math.random()*12)+1;
-//     // let num5= Math.floor(Math.random()*12)+1;
-//     // let num6= Math.floor(Math.random()*12)+1;
-//     // let num7= Math.floor(Math.random()*12)+1;
-//     // let num8= Math.floor(Math.random()*12)+1;
-// }};
-
-// function hitMe1(num1, num2){
-// how to get values from above fxn to pull to here- should it be global??
-
-//     document.getElementsByClassName(num1).textContent = values[0]
-//     document.getElementsByClassName(num2) = values[1]
-
-//     change computer card from red to black and store value somewhere for later- how to!!
-// };
-
-// function hitMe2(){
-
-// };
-
-// function hitMe3(){
-
-// };
-
-// function hitMe4(){
-
-// };
-
-// function computerRoundScore(){
-//     tot up computer cards each round- if <19 deal if > hold
-// }
+function endGame(){
+    if((player1 > 21) || (player2 > player1)){
+        alert('Computer Wins!');
+        incrementLoss();
+    } else if (player1 > player2){
+        alert('You Win!')
+        incrementScore();
+    } else {
+        alert('No Winner-Play Again!')
+        runGame();
+    }
+}
 
 // function hold(){
 //     hold player card deal to computer if <19- how to bring this here from above fxn
 // };
 
-// function playerScore(){
-//     add num 1, 3, 5 and 7- what if 5 and 7 empty??
-// };
-
-// function computerScore(){
-//     where stored??
-// };
-
-// function checkWinner(){
-//     how to define playerScore vs computerScore here from above fxn
-
-//     if (player above 21) or 
-//     if (computerScore <21 and > playerScore) {
-//         alert('Computer Wins'); 
-//         incrementLoss();
-//     }
-//     else if (computer above 21) or 
-//     if (playerScore <21 and >computerScore) {
-//         alert('You Win!'); 
-//         incrementScore();
-//     }
-//     else if (computer===player) or (both computer and player over 21) {
-//         alert('No Winner- Play Again');
-//         runGame();}
-// }
 
 /**
  * Gets score from DOM and adds 1
@@ -155,4 +79,4 @@ function incrementScore(){
 function incrementLoss(){
     let oldLoss= parseInt(document.getElementById('loss').innerText);
     document.getElementById('loss').innerText = ++oldLoss;
-};
+}
